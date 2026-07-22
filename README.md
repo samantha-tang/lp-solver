c0e081b38bf9e6cfa50a8cc2e8478d708ad9e1e9
Samantha Tang <samanthaxytang03@gmail.com>
2026-07-22 12:21:26 +1000

    Rework Problem struct and methods

--- README.md
+++ README.md
@@ -2,8 +2,56 @@
 
 A basic linear program solver in Rust.
 
+
 ## Usage
 
 This program assumes your problem is of the form,\
 $$\min_{x\in\mathbb{R_{\geqslant 0}}} c^{\intercal}x \\ \text{s.t. } Ax\geq b$$\:
 where $A\in M_{m\times n}(R)$, $b\in\mathbb{R}^m$ and $c\in\mathbb{R}^n$ for $m,n\in\mathbb{N}$.
+
+
+## Basic Doocumentation
+
+This program operates around `Problem` objects, which represent a linear
+minimisation model with an objective function, a constraint set, and variables.
+
+To build a model, use the following `Problem` methods:
+
+```
+::new()
+```
+Initialises an empty `Problem` instance.
+
+```
+.set_objective(c: Vec<T>, objtype: ObjType)
+```
+Sets the problem objective vector to `c`, where `objtype` can be `ObjType::Min`
+or `ObjType::Max`. In the latter case the program converts to an equivalent
+minimisation problem.
+
+```
+.add_var(vartype: VarType)
+```
+Adds variable of type `vartype`. This can be either `VarType::Binary`,
+`VarType::Continuous`, or `VarType::Integer`.
+
+```
+.assert()
+```
+Asserts whether the current model's objective and constraint matrices match
+in dimensions. Recall that the coefficient matrix $A$ must be of size
+$m\times n$ for some $m,n\in\mathbb{N}$, $b$ must be of length $m$, and an
+objective must be of size $n$.
+
+---
+
+To build the constraint set, either build (flattened) coefficient matrix
+`ConstraintSet.A` and vector `ConstraintSet.b` directly, or use the following
+`ConstraintSet` method to add individual constraints:
+
+```
+.add_constraint(A_i: Vec<T>, b_i: T, relation: Relation)
+```
+Adds a single constraint $A_ix\geq b_i$, $A_ix\leq b_i$, or $A_ix = b_i$ for
+relation `Ge`, `Le`, or `Eq` respectively. In the latter two cases the
+constraint converts to one or two equivalent $\geq$ constraints.
---
+++ src/problem.rs
@@ -0,0 +1,116 @@
+/// flattened matrix struct
+pub struct Matrix<T> {
+    pub m: usize,
+    pub n: usize,
+    pub data: Vec<T>,
+}
+
+impl<T> Matrix<T> {
+    fn new() -> Self {
+        Self { m: 0, n: 0, data: vec![] }
+    }
+}
+
+/// optimisation problem struct
+pub struct Problem<T> {
+    pub c: Vec<T>, //objective
+    pub constraints: ConstraintSet<T>,
+    pub vars: Vec<Variable<T>>,
+}
+
+impl<T: Clone + std::ops::Neg<Output = T>> Problem<T> {
+    fn new() -> Self {
+        Self {
+            c: vec![],
+            constraints: ConstraintSet::new(),
+            vars: vec![],
+        }
+    }
+    pub fn assert(&self) -> Result<(), String> {
+        if self.constraints.A.m != self.constraints.b.len() || self.constraints.A.n != self.c.len() {
+            Err(String::from("Inputs are not the right relative dimensions."))
+        } else {
+            Ok(())
+        }
+    }
+    pub fn set_objective(&mut self, c: Vec<T>, objtype: ObjType) {
+        match objtype {
+            ObjType::Min => {
+                self.c = c;
+            },           
+            ObjType::Max => {
+                self.c = c.iter().cloned().map(|x| -x).collect()
+            },
+
+        }
+    }
+    pub fn add_var(&mut self, vartype: VarType) {
+        let new_var = Variable::new(vartype);
+        self.vars.push(new_var);
+    }
+}
+
+pub enum ObjType {
+    Min,
+    Max,
+}
+
+pub enum Relation {
+    Ge,
+    Le,
+    Eq,
+}
+
+// constraint set, interpreted as Ax >= b
+pub struct ConstraintSet<T> {
+    pub A: Matrix<T>,
+    pub b: Vec<T>,
+}
+
+impl<T: Clone + std::ops::Neg<Output = T>> ConstraintSet<T> {
+    pub fn new() -> Self {
+        Self { A: Matrix::new(), b: vec![] }
+    }
+
+    fn push_row(&mut self, a_row: Vec<T>, b_val: T) {
+        self.A.data.extend(a_row);
+        self.A.m += 1;
+        self.b.push(b_val);
+    }
+
+    fn negate_row(a_row: &[T], b_val: &T) -> (Vec<T>, T) {
+        (a_row.iter().cloned().map(|x| -x).collect(), -b_val.clone())
+    }
+
+    pub fn add_constraint(&mut self, a_i: Vec<T>, b_i: T, relation: Relation) {
+        match relation {
+            Relation::Ge => self.push_row(a_i, b_i),
+            Relation::Le => {
+                let (neg_a, neg_b) = Self::negate_row(&a_i, &b_i);
+                self.push_row(neg_a, neg_b);
+            },
+            Relation::Eq => {
+                let (neg_a, neg_b) = Self::negate_row(&a_i, &b_i);
+                self.push_row(a_i, b_i);
+                self.push_row(neg_a, neg_b);
+            },
+        }
+    }
+}
+
+pub enum VarType {
+    Binary,
+    Continuous,
+    Integer,
+}
+
+pub struct Variable<T> {
+    pub vartype: VarType,
+    pub value: Option<T>,
+}
+
+impl<T> Variable<T> {
+    fn new(vartype: VarType) -> Self {
+        Self { vartype, value: None }
+    }
+}
